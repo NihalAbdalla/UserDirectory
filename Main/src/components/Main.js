c3d5f07e0339b37ebe1a24607038bfaec09046ec
Nihal Abdalla <miz46769@gmail.com>
2021-07-05 21:08:59 -0400

    update folders

--- Main/src/components/Main.js
+++ Main/src/components/Main.js
@@ -2,12 +2,3 @@ import React, { Component } from "react";
 import DataArea from "./DataArea";
 import "../styles/Main.css";
 
-export default class Main extends Component {
-  render() {
-    return (
-      <>
-        <DataArea />
-      </>
-    );
-  }
-}
